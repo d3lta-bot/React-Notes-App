eece714f032dbde1ab3731526dc5cc80594d27d4
nayan <nayandey00bp@gmail.com>
2022-12-04 08:00:51 +0530

    final version
    This is the final version of this Quick-Notes I guess

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,15 +1,21 @@
 import NotesItem from "./NotesItem"
 import { useContext } from "react";
 import NotesContext from "../Context/NotesContext";
+import { Card,Typography,CardBody } from "@material-tailwind/react";
 
 export default function NotesList() {
   const {notes} = useContext(NotesContext)
   return (
-    <div>
-      {notes.map((note)=>{
-      return(
-        <NotesItem note={note} key={note.id}/>
-      )})}
-    </div>
+    notes.length <1 ? <Card className="w-64 m-auto my-5 ">
+      <CardBody className="text-center">
+      <Typography variant="h5">No Notes Added Yet</Typography>
+      </CardBody>
+    </Card> : <div>
+    {notes.map((note)=>{
+    return(
+      <NotesItem note={note} key={note.id}/>
+    )})}
+  </div>
   )
 }
+
